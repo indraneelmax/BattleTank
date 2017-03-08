57411fe8d78a23a1e64c64e0ee9e82a8ac369792
DESKTOP-9S9GUOV\indra <indraneel.max@gmail.com>
2017-03-09 00:12:43 +0530

    [BattleTank-6] Add PlayerController and Tank C++ class as parents to respective BP

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PLayerController class beginPlay"));
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PLayerController not possessing Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PLayerController possessing: %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
