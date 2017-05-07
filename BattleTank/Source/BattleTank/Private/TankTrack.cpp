206e3d56792b371c856c3f911c8bb80dd127e8bb
DESKTOP-9S9GUOV\indra <indraneel.max@gmail.com>
2017-05-07 17:30:26 +0530

    [BattleTank-31] Forces to move tank applied only when track hits/is on ground !

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -6,10 +6,34 @@
 
 UTankTrack::UTankTrack()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
+
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+void UTankTrack::BeginPlay()
+{
+	//OnComponentHit is an Event which is registering to call OnHit function
+	//This OnComponentHit can be added through BP but we are doing throug C++
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);	
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	//Signature of this function found thru OnComponentHit definition -> FComponentHitSignature defintiion
+	//UE_LOG(LogTemp, Warning, TEXT("I am HIt ! I am HIt!"));
+	
+	//Previously driveTrack was inside SetThrottle func
+	//To make sure that when track is on ground we apply forces
+	//we moved out force apply code into DriveTrack
+	//And only set CurrentThrottle via TankMovementComponet
+	ApplySideWaysForce();
+	DriveTrack();
+	//We need to reset otherwise last value will be retained and tank continues
+	CurrentThrottle = 0;
+}
+
+
+void UTankTrack::ApplySideWaysForce()
 {
 	//Find the Slippage Speed i.e. (magnitude) component of velocity sideways i.e along right vector
 	auto myVelocity = GetComponentVelocity();
@@ -20,6 +44,7 @@ void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActor
 	//GetOwner()->GetRootComponent() gives us a Scene Component type.
 	//but we need our tank root "mass" which is basically on a Static MEsh Component, hence we
 	//need to type cast that to StaticMeshComponent
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
 	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
 	auto acceleration = SlippageSpeed / DeltaTime * GetRightVector(); // direction of acceleration
 	auto CorrectionForce = TankRoot->GetMass() * (-acceleration); //negated
@@ -31,15 +56,19 @@ void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActor
 
 void UTankTrack::SetThrottle(float throttle)
 {
+	CurrentThrottle += throttle;
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle, -1, 1);
+
+
+}
+void UTankTrack::DriveTrack()
+{
 	auto Name = GetName();
-	//UE_LOG(LogTemp, Warning, TEXT("%s throttle at %f"), *Name, throttle);
-	auto ForceApplied = GetForwardVector() * throttle * TankMaxDrivingForce;
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TankMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation(); //get this tracks pivot
 	//Get Root component of tank and apply force on it
 	// Root component is USceneComponent but that does not allow force to be applied
 	// Hence cast it to UPrimit (its super is UScene check class hierarchy)
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
-
 }
-
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -15,8 +15,13 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 
 private:
 	UTankTrack();
-	virtual void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
-
+	float CurrentThrottle = 0;
+	virtual void BeginPlay() override;
+	virtual void UTankTrack::ApplySideWaysForce();
+	void DriveTrack();
+	//Signature of OnHit function found thru OnComponentHit definition -> FComponentHitSignature defintiion
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float throttle);
